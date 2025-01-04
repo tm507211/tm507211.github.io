3d3220b069dd8a236d2305dc559f2fc26b6cead7
Charlie Murphy <tcm3@cs.princeton.edu>
2025-01-03 20:36:47 -0600

    Copy website from umadpl.

---
+++ expand.js
@@ -0,0 +1,6 @@
+//this code works only for a single sliding window -- should be generalized 
+$(document).on('click', '.header', function() { 
+    $('.content').slideToggle(200, function() {});
+  });
+  
+  
