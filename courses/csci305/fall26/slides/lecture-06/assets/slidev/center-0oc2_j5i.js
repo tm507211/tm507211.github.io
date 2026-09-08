2f60dc5303541c2db46e6e05bac7ff50a5c12aef
Charlie Murphy <charlie.murphy1@montana.edu>
2026-09-08 17:28:56 -0600

    Add Q2 study guide and lecture 6 and 7 slides to csci305 website.
    Signed-off-by: Charlie Murphy <charlie.murphy1@montana.edu>

---
+++ courses/csci305/fall26/slides/lecture-06/assets/slidev/center-0oc2_j5i.js
@@ -0,0 +1,1 @@
+import{O as e,T as t,X as n,q as r}from"../modules/shiki-BWHb7AAJ.js";import{t as i}from"../_plugin-vue_export-helper-BDNMzG2s.js";var a={},o={class:`slidev-layout center h-full grid place-content-center`},s={class:`my-auto`};function c(i,a){return r(),e(`div`,o,[t(`div`,s,[n(i.$slots,`default`)])])}var l=i(a,[[`render`,c]]);export{l as t};
